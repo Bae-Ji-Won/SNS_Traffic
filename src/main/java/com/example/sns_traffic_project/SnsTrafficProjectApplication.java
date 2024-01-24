384451d12ba04a2d89dfb14db8df9ac43727f89d
Bae Ji Won <qowhxk@naver.com>
2024-01-24 10:29:28 +0900

    init : Initialize project

---
+++ src/main/java/com/example/sns_traffic_project/SnsTrafficProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.sns_traffic_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SnsTrafficProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsTrafficProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sns_traffic_project/SnsTrafficProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sns_traffic_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsTrafficProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
