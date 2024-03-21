e254f68e0261169cb893ca3b15d3e440d87073f8
Bae Ji Won <qowhxk@naver.com>
2024-03-21 23:15:01 +0900

    #7 - User Controller/Service Add

--- src/main/java/com/example/sns_traffic_project/controller/UserController.java
+++ src/main/java/com/example/sns_traffic_project/controller/UserController.java
@@ -2,8 +2,10 @@ package com.example.sns_traffic_project.controller;
 
 import com.example.sns_traffic_project.dto.UserDto;
 import com.example.sns_traffic_project.dto.request.UserJoinRequest;
+import com.example.sns_traffic_project.dto.request.UserLoginRequest;
 import com.example.sns_traffic_project.dto.response.Response;
 import com.example.sns_traffic_project.dto.response.UserJoinResponse;
+import com.example.sns_traffic_project.dto.response.UserLoginResponse;
 import com.example.sns_traffic_project.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -22,7 +24,12 @@ public class UserController {
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request){
         UserDto dto = userService.join(request.userName(),request.password());
         UserJoinResponse response = UserJoinResponse.from(dto);
-        System.out.println(response);
         return Response.success(response);
+    }
+
+    @PostMapping("/login")
+    public Response<UserLoginResponse> login(@RequestBody UserLoginRequest request){
+        String token = userService.login(request.userName(),request.password());
+        return Response.success(new UserLoginResponse(token));
     }
 }
--- src/main/java/com/example/sns_traffic_project/service/UserService.java
+++ src/main/java/com/example/sns_traffic_project/service/UserService.java
@@ -6,7 +6,10 @@ import com.example.sns_traffic_project.dto.UserDto;
 import com.example.sns_traffic_project.exception.ErrorCode;
 import com.example.sns_traffic_project.exception.SnsApplicationException;
 import com.example.sns_traffic_project.repository.UserRepository;
+import com.example.sns_traffic_project.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,30 +19,40 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
     private final UserRepository userRepository;
 
+    private final BCryptPasswordEncoder encoder;
 
-    // Todo : implement
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.token.expired-time-ms}")
+    private Long expiredTimeMs;
+
     public UserDto join(String userName, String pw) {
         // 유저 이름을 통한 정보 찾기 만약 있다면 에러 발생
         userRepository.findByUserName(userName).ifPresent(it -> {
             throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME,String.format("%s is duplicated",userName));
         });
 
-        User user = userRepository.save(User.of(userName, pw));
+        User user = userRepository.save(User.of(userName, encoder.encode(pw)));
         return UserDto.from(user);
     }
 
     // Todo : implement
+    @Transactional(readOnly = true)
     public String login(String userName, String password){
         // 회원가입 여부 체크
-        User user = userRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME,""));
+        User user = userRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND,String.format("%s not found",userName)));
 
         // 비밀번호 체크
-        if(!user.getPassword().equals(password)){
-            throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME,"");
+        // encoder에서 지원해주는 mathces를 사용하여 암호화된 비밀번호와 입력받은 비밀번호를 비교함
+        if(!encoder.matches(password, user.getPassword())){
+            throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD);
         }
 
         // 토큰 생성
-        
-        return "";
+        String token = JwtTokenUtils.generateToken(userName,secretKey,expiredTimeMs);
+
+        return token;
     }
 }
