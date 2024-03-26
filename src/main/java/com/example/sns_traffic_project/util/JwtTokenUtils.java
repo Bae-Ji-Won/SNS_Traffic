7b4eccc22f961e20f27421dcb83b21616b0156f7
Bae Ji Won <qowhxk@naver.com>
2024-03-26 20:03:51 +0900

    #6  Security - Exception Add
    Jwt  예외처리 메서드 추가
    1. 현재 로그인한 유저의 이름 찾는 메서드 추가
    2. 토큰 시간 만료되었는지 확인하는 메서드 추가
    3. 외부에서 받아온 토큰이 우리가 발행한 토큰인지 확인하는 메서드 추가

--- src/main/java/com/example/sns_traffic_project/util/JwtTokenUtils.java
+++ src/main/java/com/example/sns_traffic_project/util/JwtTokenUtils.java
@@ -8,9 +8,28 @@ import io.jsonwebtoken.security.Keys;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
+import java.util.Scanner;
 
 public class JwtTokenUtils {
 
+
+    public static String getuserName(String token,String key){
+        return extractClaims(token,key).get("userName",String.class);
+    }
+
+    // 토큰 시간 만료되었는지 확인
+    public static boolean isExpired(String token,String key){
+        Date expiredDate = extractClaims(token,key).getExpiration();
+        return expiredDate.before(new Date());
+    }
+
+    // 외부에서 받아온 토큰을 우리가 발행한 토큰인지 확인하기 위해 Key 값을 비교함
+    private static Claims extractClaims(String token, String key){
+        return Jwts.parserBuilder().setSigningKey(getKey(key))      // 외부 토큰 키값과 내가 발행한 키값이 같은지 비교 다를시, 예외 발생
+                .build().parseClaimsJws(token).getBody();           // 키 값이 같다면 Claims 객체 생성 (외부에서 받아온 토큰안에 들어있는 정보를 담고 있음)
+    }
+
+    // 토큰에 정보 넣기
     public static String generateToken(String userName, String key, long expiredTimeMs){
         // claims에 username 저장
         Claims claims = Jwts.claims();
