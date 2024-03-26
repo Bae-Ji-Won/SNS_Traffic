447b188ccfe857ffa2e83b5fd73fb5b7894ea098
Bae Ji Won <qowhxk@naver.com>
2024-03-26 19:55:19 +0900

    #7 Security - AuthenticationConfig Update
    1. UserService Dependency
    2. api permitall update
    3. filterbefore jwttokenfilter update

--- src/main/java/com/example/sns_traffic_project/config/AuthenticationConfig.java
+++ src/main/java/com/example/sns_traffic_project/config/AuthenticationConfig.java
@@ -1,9 +1,11 @@
 package com.example.sns_traffic_project.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import com.example.sns_traffic_project.config.filter.JwtTokenFilter;
+import com.example.sns_traffic_project.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,14 +14,19 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AuthenticationConfig{
+
+    private final UserService userService;
+    @Value("${jwt.secret-key}")
+    private String key;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeRequests(auth -> auth
-                        .antMatchers("/api/*/users/join","api/*/users/login").permitAll()       // 해당 api 모두 허용
+                        .antMatchers("/api/*/users/join","/api/*/users/login").permitAll()       // 해당 api 모두 허용
                         .antMatchers("/api/**").authenticated()     // 위의 api를 제외한 나머지는 모두 인증 필요
                 )
                 // 세션 따로 관리 안함
@@ -27,7 +34,8 @@ public class AuthenticationConfig{
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 // 로그인할때 유저정보를 토큰에 받아오기 위한 설정
-                .addFilterBefore(new JwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
+                // 직접 만든 JwtTokenFilter 클래스를 UsernamePasswordAuthenticationFilter 클래스보다 먼저 실행함
+                .addFilterBefore(new JwtTokenFilter(key,userService), UsernamePasswordAuthenticationFilter.class)
                 .build();
                 // security에서 인증하다 에러 발생시 handling
                 // TODO
