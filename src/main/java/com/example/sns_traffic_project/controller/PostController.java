4f1d033ddbb1599aef21f9a5b78eaa7de7c4a77d
Bae Ji Won <qowhxk@naver.com>
2024-03-26 19:58:46 +0900

    #5 Post - Authentication add

--- src/main/java/com/example/sns_traffic_project/controller/PostController.java
+++ src/main/java/com/example/sns_traffic_project/controller/PostController.java
@@ -4,6 +4,7 @@ import com.example.sns_traffic_project.dto.request.PostCreateRequest;
 import com.example.sns_traffic_project.dto.response.Response;
 import com.example.sns_traffic_project.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,8 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public Response<Void> create(@RequestBody PostCreateRequest request){
-        postService.create(request.title(), request.body(), "");
+    public Response<Void> create(@RequestBody PostCreateRequest request, Authentication authentication){
+        postService.create(request.title(), request.body(), authentication.getName());
         return Response.success(null);
     }
 }
