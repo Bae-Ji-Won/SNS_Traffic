4d0ee87025ff4151a36a075401ae269cce706908
Bae Ji Won <qowhxk@naver.com>
2024-03-26 20:02:27 +0900

    #6  Security

--- src/main/java/com/example/sns_traffic_project/SnsTrafficProjectApplication.java
+++ src/main/java/com/example/sns_traffic_project/SnsTrafficProjectApplication.java
@@ -2,7 +2,6 @@ package com.example.sns_traffic_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
